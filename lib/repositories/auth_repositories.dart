dc8b1726214d859e6543ce06564bee69883d9992
manisha <manishaa8981@gmail.com>
2024-02-17 21:25:34 +0545

    favourite

--- lib/repositories/auth_repositories.dart
+++ lib/repositories/auth_repositories.dart
@@ -1,26 +1,25 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+
 import '../model/user_model.dart';
 import '../services/firebase_service.dart';
 
 class AuthRepository {
   CollectionReference<UserModel> userRef =
-  FirebaseService.db.collection("users").withConverter<UserModel>(
-    fromFirestore: (snapshot, _) {
-      return UserModel.fromFirebaseSnapshot(snapshot);
-    },
-    toFirestore: (model, _) => model.toJson(),
-  );
+      FirebaseService.db.collection("users").withConverter<UserModel>(
+            fromFirestore: (snapshot, _) {
+              return UserModel.fromFirebaseSnapshot(snapshot);
+            },
+            toFirestore: (model, _) => model.toJson(),
+          );
   Future<UserCredential?> register(UserModel user) async {
     try {
-      final response =
-      await userRef.where("name", isEqualTo: user.name!).get();
+      final response = await userRef.where("username", isEqualTo: user.username!).get();
       if (response.size != 0) {
         throw Exception("Username already exists");
       }
       UserCredential uc = await FirebaseService.firebaseAuth
-          .createUserWithEmailAndPassword(
-          email: user.email!, password: user.password!);
+          .createUserWithEmailAndPassword(email: user.email!, password: user.password!);
 
       user.userId = uc.user!.uid;
       user.fcm = "";
@@ -44,11 +43,12 @@ class AuthRepository {
 
   Future<UserModel> getUserDetail(String id, String? token) async {
     try {
-
+      // final response = await userRef
+      //     .where("user_id", isEqualTo: id).get();
       final response = await userRef.doc(id).get();
       var user = response.data()!;
       user.fcm = token ?? "";
-      await userRef.doc(user.userId).set(user);// yha u.id thyo maila u.userid grya xu
+      await userRef.doc(user.id).set(user);
       return user;
     } catch (err) {
       rethrow;
@@ -57,8 +57,7 @@ class AuthRepository {
 
   Future<bool> resetPassword(String email) async {
     try {
-      var res = await FirebaseService.firebaseAuth
-          .sendPasswordResetEmail(email: email);
+      var res = await FirebaseService.firebaseAuth.sendPasswordResetEmail(email: email);
       return true;
     } catch (err) {
       rethrow;
