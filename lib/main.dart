4d90b03f740077fd83c68e188d6ea4b8c0e7ab44
manisha <manishaa8981@gmail.com>
2024-02-18 21:14:54 +0545

    added some images

--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,120 @@
-import 'package:candel/screens/auth/%20resetPassword.dart';
-import 'package:candel/screens/auth/login_screen.dart';
-import 'package:candel/screens/auth/register_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:loader_overlay/loader_overlay.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+import 'services/local_notification_service.dart';
+import 'viewmodels/auth_viewmodel.dart';
+import 'viewmodels/global_ui_viewmodel.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
+
+    options: DefaultFirebaseOptions.currentPlatform,
+
   );
-      runApp(MyApp());
+  NotificationService.initialize();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Candle',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        textTheme: GoogleFonts.aBeeZeeTextTheme(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider (create: (_) => GlobalUIViewModel()),
+        ChangeNotifierProvider (create: (_) => AuthViewModel()),
+        ChangeNotifierProvider (create: (_) => CategoryViewModel()),
+        ChangeNotifierProvider (create: (_) => ProductViewModel()),
+      ],
+      child: GlobalLoaderOverlay(
+        useDefaultLoading: false,
+        overlayWidget: Center(
+          child: Image.asset("assets/images/loader.gif", height: 100, width: 100,),
+        ),
+        child: Consumer<GlobalUIViewModel>(
+            builder: (context, loader, child) {
+              if(loader.isLoading){
+                context.loaderOverlay.show();
+              }else{
+                context.loaderOverlay.hide();
+              }
+              return MaterialApp(
+                title: 'Flutter Demo',
+                debugShowCheckedModeBanner: false,
+                theme: ThemeData(
+                  primarySwatch: Colors.green,
+                  textTheme: GoogleFonts.aBeeZeeTextTheme(),
+                ),
+                initialRoute: "/splash",
+                routes: {
+                  "/login": (BuildContext context)=>LoginScreen(),
+                  "/splash": (BuildContext context)=>SplashScreen(),
+                  "/register": (BuildContext context)=>RegisterScreen(),
+                  "/forget-password": (BuildContext context)=>ForgetPasswordScreen(),
+                  "/dashboard": (BuildContext context)=>DashboardScreen(),
+                  "/add-product": (BuildContext context)=>AddProductScreen(),
+                  "/edit-product": (BuildContext context)=>EditProductScreen(),
+                  "/single-product": (BuildContext context)=>SingleProductScreen(),
+                  "/single-category": (BuildContext context)=>SingleCategoryScreen(),
+                  "/my-products": (BuildContext context)=>MyProductScreen(),
+                },
+              );
+            }
+        ),
       ),
-      initialRoute: "/login",
-      routes: {
-    "/login":(BuildContext context) => LoginScreen(),
-        "/register":(BuildContext context) => RegisterScreen(),
-        "/resetPassword":(BuildContext context) => resetPassword()
-    },
     );
   }
 }
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
